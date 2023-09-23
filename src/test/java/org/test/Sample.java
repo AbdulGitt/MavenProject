3dba3f504dd845969afdbe928d4e0f52db49dd74
AbdulGitt <greenstechnology12345@gmail.com>
2023-09-23 11:44:26 +0530

    Project structure

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+import org.base.BaseClass;
+import org.junit.Test;
+
+public class Sample extends BaseClass{
+	
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+
+}
